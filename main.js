14897d653c15e324bf317ad9c1df9366d9647932
DHIRAJ SINGHA <76531276+dhirajsingha143@users.noreply.github.com>
2022-12-22 11:39:35 +0100

    Fix JS script functionality issues

--- main.js
+++ main.js
@@ -21,233 +21,319 @@ navLink.addEventListener('click', () => {
   openMenu();
 });
 
-//pop-up window
+//pop-up window portfolio-project section
 
-/* Project list */
-const listProject = [
-  {
-    name: 'Tonic',
-    description: 'A daily section of privately personalized reads; no accounts or sign-ups required.',
-    image: 'images/portfolio-snapshot-1.svg',
-    details: ['CANOPY', 'Back End Dev', '2015'],
-    technologies: ['html', 'css', 'javaScript'],
-    link: { live: '', source: '' },
-  },
-  {
-    name: 'Multi-Post Stories', description: 'A daily section of privately personalized reads; no accounts or sign-ups required.', image: 'images/portfolio- snapshot-2.svg', details: ['CANOPY', 'Back End Dev', '2015'], technologies: ['html', 'css', 'javaScript'], link: { live: '', source: '' },
-  },
-  {
-    name: 'Tonic', description: 'A daily section of privately personalized reads; no accounts or sign-ups required.', image: 'images/portfolio- snapshoot-3.svg', details: ['CANOPY', 'Back End Dev', '2015'], technologies: ['html', 'css', 'javaScript'], link: { live: '', source: '' },
-  },
-  {
-    name: 'Multi-Post Stories', description: 'A daily section of privately personalized reads; no accounts or sign-ups required.', image: 'images/portfolio-snapshoot-4.svg', details: ['CANOPY', 'Back End Dev', '2015'], technologies: ['html', 'css', 'javaScript'], link: { live: '', source: '' },
-  },
-];
+function showDetails(project) {
+  //containers for popup-window
+  const popupcontainer = document.createElement('div');
+  popupcontainer.classList.add('popup-container');
+  popupcontainer.id='popup-container';
+  const popupimgContainer = document.createElement('div');
+  popupimgContainer.classList.add('popupimg-container');
 
-function workCard(project,id) {
-  const workSection = document.querySelector('#work');
+  //img
+  const imgDiv = document.createElement('div');
+  imgDiv.classList.add('image-container');
+  const img = document.createElement('img');
+  img.classList.add('img');
+  img.src =project.image;
+  img.alt = 'Portfolio Project Image';
+  imgDiv.append(img);
+  //div.image-container>
+  //img.img>scr="project.image">alt="Portfolio Project Image"
 
+  const btnContainer = document.createElement('div');
+  btnContainer.classList.add('btn-container');
 
-  const card = `
+  //Title
+  const title = document.createElement('h2');
+  title.classList.add('project-title');
+  title.textContent = project.name;
+  btnContainer.append(title);
+  //h2.project-title> project.name
 
-  <div class="card" >
-  <img src="images/Project-1.png" alt="Screenshot of Tonic web page" />
-  <div class="card-info-container">
-    <h2 class="title">${project.name}</h2>
-    <ul class="info">
-      <li class="info-items">${project.details[0]}</li>
-      <li class="info-item"><i class="fa-solid fa-circle fa-xs"></i></li>
-      <li class="info-items">${project.details[1]}</li>
-      <li class="info-item"><i class="fa-solid fa-circle fa-xs"></i></li>
-      <li class="info-items">${project.details[2]}</li>
-    </ul>
-    <p class="card-text">A daily selection of privately personalized reads; no accounts or sign-ups required.</p>
-    <ul class="program">
-      <li class="language">${project.technologies[0]}</li>
-      <li class="language">${project.technologies[1]}</li>
-      <li class="language">${project.technologies[2]}</li>
-    </ul>
-    <button id=${id} class="btn modal">See Project</button>
-  </div>
-</div>
+  //btnClose
+  const btnClose = document.createElement('i');
+  btnClose.classList.add('fa-solid','fa-x');
+  btnClose.addEventListener('click', () => {
+    document.getElementById('popup-container').remove();
+  });
+  btnContainer.append(btnClose);
+  //div.btn-container> img#close-btn>src="images/cross2.svg">alt...
+  //event = btnClose-> #popup-container.remove()
 
-`;
+  //List of Details-------------------------------------
+  const projectName = document.createElement('ul');
+  projectName.classList.add('project-info');
+  //similar to above list of details
 
-  workSection.innerHTML += card;
+  let j = 0;
+  const subTitle = document.createElement('li');
+  subTitle.classList.add('sub-title');
+  subTitle.textContent = project.details[j];
 
+  const bullet1 = document.createElement('li');
+  bullet1.classList.add('bullet');
+  const symbol1 = document.createElement('i');
+  symbol1.classList.add('fa-solid','fa-circle','fa-xs');
+  bullet1.append(symbol1);
+  //-----------------
+  const bullet2 = document.createElement('li');
+  bullet2.classList.add('bullet');
+  const symbol2 = document.createElement('i');
+  symbol2.classList.add('fa-solid','fa-circle','fa-xs');
+  bullet2.append(symbol2);
+
+  j += 1;
+  const typeProject = document.createElement('li');
+  typeProject.classList.add('backend-yr');
+  typeProject.textContent = project.details[j];
+
+  j += 1;
+  const date = document.createElement('li');
+  date.classList.add('backend-yr');
+  date.textContent = project.details[j];
+
+  projectName.append(subTitle);
+  projectName.append(bullet1);
+  projectName.append(typeProject);
+  projectName.append(bullet2);
+  projectName.append(date);
+  //ul.project-name
+    //li.sub-title> a>href='#'<- project.details[j] {Cannopy}
+    //li.bullet(&#x2022) {*}
+    //li> a.backend-yr>href="#"<- project.details[j] {Backend Dev}
+    //li.bullet(&#x2022) {*}
+    //li> a.backend-yr <-project.details[j] {2015}
+
+  // Descriptions
+  const description = document.createElement('p');
+  description.classList.add('description');
+  description.textContent = project.description;
+  //p.discription <- project.description
+
+  // detail Footer
+  const detailFooter = document.createElement('div');
+  detailFooter.classList.add('detail-footer');
+  //div.detail-footer
+
+  // list techonologies
+  const technologies = document.createElement('ul');
+  technologies.classList.add('program');
+  //ul.classification
+  for (let i = 0; i < project.technologies.length; i += 1) {
+    const li = document.createElement('li');
+    li.classList.add('language');
+    const anchor = document.createElement('a');
+    anchor.href = '#';
+    anchor.textContent = project.technologies[i];
+    li.append(anchor);
+    technologies.append(li);
+  }
+  //ul.classification > li.lang> a>href="#" <- project.technologies[i]
+
+  // Buttons
+  const buttonLive = document.createElement('button');
+  buttonLive.classList.add('btn');
+  buttonLive.type = 'button';
+  //button.btn>type="button"
+
+  const txtBtnLive = document.createElement('p');
+  txtBtnLive.textContent = 'See Live';
+  //p <- "See Live"
+
+  const imgBtnLive = document.createElement('img');
+  imgBtnLive.src = 'images/live-Icon.svg';
+  imgBtnLive.alt = 'Button Live';
+  buttonLive.append(txtBtnLive);
+  buttonLive.append(imgBtnLive);
+  //button.btn>type="button">p <- "See Live">img>src..>alt..
+
+  const buttonSource = document.createElement('button');
+  buttonSource.classList.add('btn');
+  buttonSource.type = 'button';
+
+  const txtBtnSource = document.createElement('p');
+  txtBtnSource.textContent = 'See Source';
+
+  const imgBtnSource = document.createElement('img');
+  imgBtnSource.src = 'images/purple-github in button.svg';
+  imgBtnSource.alt = 'Button Source';
+  buttonSource.append(txtBtnSource);
+  buttonSource.append(imgBtnSource);
+  //button.btn>type="button">p <- "See Source">img>src..>alt..
+
+  
+  detailFooter.append(buttonLive);
+  detailFooter.append(buttonSource);
+  //div.detail-footer >
+    //ul.classification > li.lang> a>href="#" <- project.technologies[i]
+    //button.btn>type="button">p <- "See Live">img>src..>alt..
+    //button.btn>type="button">p <- "See Source">img>src..>alt..
+
+  // Add all element into the container
+  // popupimgContainer.append(title);
+  popupimgContainer.append(btnContainer);
+  popupimgContainer.append(projectName);
+  popupimgContainer.append(imgDiv);
+  popupimgContainer.append(description);
+  popupimgContainer.append(technologies);
+  popupimgContainer.append(detailFooter);
+
+  popupcontainer.append(popupimgContainer);
+  document.body.append(popupcontainer);
+
+  //All elements append sequence wise inside body. 
+
+  /*popupimgcontainer.popupimg-container
+    h2.project-title> project.name
+    div.btn-container> img#close-btn>src="images/cross2.svg">alt...{EVENT}
+        ul.project-name
+            li.sub-title> a>href='#'<- project.details[j] {Cannopy}
+            li.bullet(&#x2022) {*}
+            li> a.backend-yr>href="#"<- project.details[j] {Backend Dev}
+            li.bullet(&#x2022) {*}
+            li> a.backend-yr <-project.details[j] {2015}
+    div.image-container>
+        img.img>scr="project.image">alt="Portfolio Project Image"
+        p.discription <- project.description
+    div.detail-footer >
+        ul.classification > 
+            li.lang> a>href="#" <- project.technologies[i] {Loop}
+    button.btn>type="button">p <- "See Live">img>src..>alt..
+    button.btn>type="button">p <- "See Source">img>src..>alt..
+  */
 }
 
-// // a UI class
-class UI {
-  static displayUI(project) {
-    const wrapperDiv = document.createElement('div');
-    wrapperDiv.classList = 'popup-wrapper';
-    wrapperDiv.id = 'popup-wrapper';
+/* Update list of project dynamicaly */
+const defaultDescription = 'A daily section of privately personalized reads; no accounts or sign-ups required.'
+const listProject = [
+    {
+      name: 'Tonic', description: defaultDescription , image: 'images/Project-1.png', details: ['CANOPY', 'Back End Dev', '2015'], technologies: ['html', 'css', 'javaScript'], link: { live: '', source: '' },
+    },
+    {
+      name: 'Multi-Post Stories',  description: defaultDescription , image: 'images/Project-2.png', details: ['CANOPY', 'Back End Dev', '2015'], technologies: ['html', 'css', 'javaScript'], link: { live: '', source: '' },
+    },
+    {
+      name: 'Facebook 360', description: defaultDescription , image: 'images/Project-3.png', details: ['CANOPY', 'Back End Dev', '2015'], technologies: ['html', 'css', 'javaScript'], link: { live: '', source: '' },
+    },
+    {
+      name: 'Uber Navigation', description: defaultDescription , image: 'images/Project-4.png', details: ['CANOPY', 'Back End Dev', '2015'], technologies: ['html', 'css', 'javaScript'], link: { live: '', source: '' },
+    },
+  ];
 
-    //portfolio snapshot
-    const portfolioSnapshot = document.createElement('div');
-    portfolioSnapshot.classList.add('portfolio-snapshot');
+const work = document.getElementById('portfolio');
+let index = 0;
+listProject.forEach((project) => {
+  const portfolioCard = document.createElement('div');
+  portfolioCard.classList.add('card');
+  //div.card
 
-    //img div
-    const imgDiv = document.createElement('div');
-    imgDiv.classList.add('img-wrapper');
-    const image = document.createElement('img');
-    image.classList.add('img');
-    image.src = project.image;
-    image.alt = 'portfolio image';
-    imgDiv.append(image);
+  const img = document.createElement('img');
+  img.classList.add('img');
+  img.src = project.image;
+  img.alt = 'Portfolio-Project-1';
+  //img.img>src="project.image">alt..
 
-    //title
-    const title = document.createElement('h1');
-    title.classList.add('project-title');
-    title.textContent = project.name;
+  const workDescription = document.createElement('div');
+  workDescription.classList.add('card-info-container');
+  //div.work-description
 
-    //button close
-    const btnWrapper = document.createElement('div');
-    btnWrapper.classList.add('btn-wrapper');
-    const btnClose = document.createElement('img');
-    btnClose.id = 'close-btn';
-    btnClose.src = 'images/cross2.svg';
-    btnClose.alt = 'Close button';
-    btnClose.addEventListener('click', () => {
-      document.getElementById('popup-wrapper').remove();
-    });
-    btnWrapper.append(btnClose);
+  const title = document.createElement('h2');
+  title.classList.add('title');
+  title.textContent = project.name;
+  //h2.project-title <- project.name
 
-    /* List of details */
-    const projectName = document.createElement('ul');
-    projectName.classList.add('project-name');
+  /* List of details */
+  const projectName = document.createElement('ul');
+  projectName.classList.add('info');
+  //similar to above list of details
 
-    let j = 0;
-    const subTitle = document.createElement('li');
-    subTitle.classList.add('sub-title');
-    const subTitleAnchor = document.createElement('a');
-    subTitleAnchor.href = '#';
-    subTitleAnchor.textContent = project.details[j];
-    subTitle.append(subTitleAnchor);
+  let j = 0;
+  const subTitle = document.createElement('li');
+  subTitle.classList.add('sub-title');
+  subTitle.textContent = project.details[j];
 
-    const bullet1 = document.createElement('li');
-    bullet1.classList.add('bullet');
-    bullet1.innerHTML = '&#x2022;';
-    const bullet2 = document.createElement('li');
-    bullet2.classList.add('bullet');
-    bullet2.innerHTML = '&#x2022;';
+  const bullet1 = document.createElement('li');
+  bullet1.classList.add('bullet');
+  const symbol1 = document.createElement('i');
+  symbol1.classList.add('fa-solid','fa-circle','fa-xs');
+  bullet1.append(symbol1);
+  //-----------------
+  const bullet2 = document.createElement('li');
+  bullet2.classList.add('bullet');
+  const symbol2 = document.createElement('i');
+  symbol2.classList.add('fa-solid','fa-circle','fa-xs');
+  bullet2.append(symbol2);
 
-    j += 1;
-    const typeProject = document.createElement('li');
-    const typeProjectAnchor = document.createElement('a');
-    typeProjectAnchor.classList.add('backend-yr');
-    typeProjectAnchor.href = '#';
-    typeProjectAnchor.textContent = project.details[j];
-    typeProject.append(typeProjectAnchor);
+  j += 1;
+  const typeProject = document.createElement('li');
+  typeProject.classList.add('backend-yr');
+  typeProject.textContent = project.details[j];
 
-    j += 1;
-    const date = document.createElement('li');
-    const dateAnchor = document.createElement('a');
-    dateAnchor.classList.add('backend-yr');
-    dateAnchor.textContent = project.details[j];
-    date.append(dateAnchor);
+  j += 1;
+  const date = document.createElement('li');
+  date.classList.add('backend-yr');
+  date.textContent = project.details[j];
 
-    projectName.append(subTitle);
-    projectName.append(bullet1);
-    projectName.append(typeProject);
-    projectName.append(bullet2);
-    projectName.append(date);
+  projectName.append(subTitle);
+  projectName.append(bullet1);
+  projectName.append(typeProject);
+  projectName.append(bullet2);
+  projectName.append(date);
 
-    // Descriptions
-    const description = document.createElement('p');
-    description.classList.add('description');
-    description.textContent = project.description;
-    /* */
+  //appending all li's into ul.project.name
 
-    // detail Footer
-    const detailFooter = document.createElement('div');
-    detailFooter.classList.add('detail-footer');
+  // Descriptions of project in card
+  const description = document.createElement('p');
+  description.classList.add('card-text');
+  description.textContent = project.description;
 
-    // list techonologies
-    const technologies = document.createElement('ul');
-    technologies.classList.add('classification');
-    for (let i = 0; i < project.technologies.length; i += 1) {
-      const li = document.createElement('li');
-      li.classList.add('lang');
-      const anchor = document.createElement('a');
-      anchor.href = '#';
-      anchor.textContent = project.technologies[i];
-      li.append(anchor);
-      technologies.append(li);
-    }
-
-    // Buttons
-    const buttonLive = document.createElement('button');
-    buttonLive.classList.add('btn');
-    buttonLive.type = 'button';
-
-    const txtBtnLive = document.createElement('p');
-    txtBtnLive.textContent = 'See Live';
-
-    const imgBtnLive = document.createElement('img');
-    imgBtnLive.src = 'images/live-Icon.svg';
-    imgBtnLive.alt = 'Button Live';
-    buttonLive.append(txtBtnLive);
-    buttonLive.append(imgBtnLive);
-
-    const buttonSource = document.createElement('button');
-    buttonSource.classList.add('btn');
-    buttonSource.type = 'button';
-
-    const txtBtnSource = document.createElement('p');
-    txtBtnSource.textContent = 'See Source';
-
-    const imgBtnSource = document.createElement('img');
-    imgBtnSource.src = 'images/purple-github in button.svg';
-    imgBtnSource.alt = 'Button Source';
-    buttonSource.append(txtBtnSource);
-    buttonSource.append(imgBtnSource);
-
-    detailFooter.append(technologies);
-    detailFooter.append(buttonLive);
-    detailFooter.append(buttonSource);
-
-    // Add all element into the container
-    portfolioSnapshot.append(title);
-    portfolioSnapshot.append(btnWrapper);
-    portfolioSnapshot.append(projectName);
-    portfolioSnapshot.append(imgDiv);
-    portfolioSnapshot.append(description);
-    portfolioSnapshot.append(detailFooter);
-
-
-    wrapperDiv.append(portfolioSnapshot);
-
-
-
-    console.log(wrapperDiv);
-
-    //add to the body
-    // document.querySelector('body').appendChild(wrapperDiv);
-    //or better 
-    document.body.append(wrapperDiv);
-
-
+  //creating list of technology languages used
+  const technologies = document.createElement('ul');
+  technologies.classList.add('program');
+  for (let i = 0; i < project.technologies.length; i += 1) {
+    const li = document.createElement('li');
+    li.classList.add('language');
+    const anchor = document.createElement('a');
+    anchor.href = '#';
+    anchor.textContent = project.technologies[i];
+    li.append(anchor);
+    technologies.append(li);
   }
 
-}
+  // btn for the popup-showdetailed project
+  const buttonDiv = document.createElement('div');
+  const button = document.createElement('button');
+  button.classList.add('btn');
+  button.type = 'button';
+  button.textContent = 'See Project';
 
-//print cards
-listProject.forEach( (project,id) => {
-  workCard(project,id);
- 
-})
+  // On click show detail
+  button.addEventListener('click', () => { 
+    showDetails(project); 
+  });
 
+  buttonDiv.append(button);
 
+  // add all elements 
+  workDescription.append(title);
+  workDescription.append(projectName);
+  workDescription.append(description);
+  workDescription.append(technologies);
+  workDescription.append(buttonDiv);
 
-//added event listener to each modal on card
-const modals = document.querySelectorAll('.modal');
+  //This function helps in reversing the order of elements in cards
+  if (index % 2 === 0) {
+    portfolioCard.append(img);
+    portfolioCard.append(workDescription);
+  } else {
+    portfolioCard.append(img);
+    portfolioCard.append(workDescription);
+    portfolioCard.classList.add('card-reverse');
+  }
 
-
-modals.forEach(modal => {
-  modal.addEventListener('click', (e) => {
-    UI.displayUI(listProject[e.target.id]);
-    console.log(e.target.id)
-  })
-})
+  // add project to the work section
+  work.append(portfolioCard);
+  index += 1;
+});
